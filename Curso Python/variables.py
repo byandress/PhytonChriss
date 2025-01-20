c83f674bfd532ffdc8b63736cc04f970f3021747
ChrisBarone <133384601+ChrisBaroneYT@users.noreply.github.com>
2025-01-20 10:53:41 -0500

    Variables en PY

--- Curso Python/variables.py
+++ Curso Python/variables.py
@@ -2,3 +2,8 @@ miVariable = 10
 nombre = "Juan"
 precio = 19.99
 esActivo = True
+
+edad = 30               # int
+nombre = "Ana"          # str
+altura = 1.75           # float
+es_estudiante = True    # bool

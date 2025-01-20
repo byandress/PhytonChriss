af4aa8cc4657d21806796dd3c566e3a3a38161d0
ChrisBarone <133384601+ChrisBaroneYT@users.noreply.github.com>
2025-01-20 10:55:40 -0500

    Operadores en Py

---
+++ Curso Python/operadores.py
@@ -0,0 +1,10 @@
+a = 10
+b = 3
+
+print(a + b)  # Suma: 13
+print(a - b)  # Resta: 7
+print(a * b)  # Multiplicación: 30
+print(a / b)  # División: 3.3333333333333335
+print(a // b) # División entera: 3
+print(a % b)  # Módulo: 1
+print(a ** b) # Exponentiación: 1000
